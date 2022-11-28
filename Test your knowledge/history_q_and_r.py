112c349a5fc31199f90f81518021ab783011562c
Enes <enes.barut@gmx.com>
2022-11-28 17:48:37 +0100

    added high low and test your knowledge files

---
+++ Test your knowledge/history_q_and_r.py
@@ -0,0 +1,115 @@
+questions = [
+
+    {'question': "Which queen had the shortest reign of Henry VIII’s six wives?"},
+    {'question': "In 16th-century Japan, who was Yasuke?"},
+    {'question': "Who wrote the 12th-century account Historia regum Britanniae (The History of the Kings of Britain), "
+                 "which is often credited with making the legend of King Arthur popular?"},
+    {'question': "It is thought that Harriet Tubman directly rescued around 3,00 people from slavery and gave "
+                 "instructions to help dozens more. But in which conflict did she become the first woman to lead an "
+                 "armed assault?"},
+    {'question': "In which country is the Bay of Pigs?"},
+    {'question': "Which medieval queen was married to both Louis VII of France and Henry II of England?"},
+    {'question': "Who was the first human to journey into space?"},
+    {'question': "Whose body was exhumed from Westminster Abbey, more than two years after his death, "
+                 "to be ‘executed’ for treason?"},
+    {'question': "Who ultimately succeeded King Alfred the Great as ‘king of the Anglo-Saxons’?"},
+    {'question': "By what nickname is Edward Teach better known?"},
+    {'question': "Julius Caesar was assassinated on 15 March 44 BC, a date now often known by what term?"},
+    {'question': "Where did the Great Fire of London begin, on 2 September 1666?"},
+    {'question': "What German dance, which sees partners spinning together in close contact, was condemned as "
+                 "depraved when it was first seen in Regency society?"},
+    {'question': "Which king preceded Queen Victoria?"},
+    {'question': "Guy Bailey, Roy Hackett and Paul Stephenson made history in 1963, as part of a protest against a "
+                 "bus company that refused to employ black and Asian drivers in which UK city?"},
+    {'question': "Who famously duelled Alexander Hamilton on 11 July 1804, resulting in the founding father’s death?"},
+    {'question': "What, in the 16th and 17th centuries, was a ‘drunkard’s cloak’?"},
+    {'question': "What is considered the world’s oldest writing system?"},
+    {'question': "Who was the mother of Emperor Nero and the wife of Emperor Claudius?"},
+    {'question': "Which pioneer of hair products became America’s first black female millionaire?"},
+    {'question': "What was Mary Anning (1799–1847) famous for?"},
+    {'question': "Who gave Queen Elizabeth I the soubriquet ‘Gloriana’?"},
+    {'question': "Although never taking her seat, who was the first woman to e elected to the houses of parliament?"},
+    {'question': "Where was Napoleon Bonaparte born?"},
+    {'question': "Can you name the five beach codenames used by Allied forces on D-Day?"},
+    {'question': "Where was the first British colony in the Americas?"},
+    {'question': "In August 1819, around 60,000 peaceful pro-democracy protestors were attacked in an open square in "
+                 "Manchester. This event was known as..."},
+    {'question': "Which rock band formed in 1994 takes its name from a term used by the Allies in the Second World "
+                 "War to describe various UFOs?"},
+    {'question': "In which year did Emily Wilding Davison die as a result of a collision with King George V’s horse "
+                 "during the Epsom Derby?"},
+    {'question': "In medieval history, what was a 'schiltron'?"},
+    {'question': "Which English king died in 1066, leaving no heir to the throne?"},
+    {'question': "Neil Armstrong, Buzz Aldrin and…? Who was the third astronaut involved in the Apollo 11 mission "
+                 "that landed on the moon?"},
+    {'question': "What was Matthew Hopkins famous for in the 17th century?"},
+    {'question': "In what century did the Peasants’ Revolt take place?"},
+    {'question': "During the US civil rights movement in the 1960s, who said: “We declare our right on this "
+                 "earth...to be a human being...by any means necessary?"},
+    {'question': "Who was the wife of the future Henry VIII’s older brother, Arthur?"},
+    {'question': "What is trepanning?"},
+    {'question': "In which decade did the potato famine strike Ireland?"},
+    {'question': "Who led the Scottish army to victory over the English at the battle of Bannockburn in 1314?"},
+    {'question': "What were the four humours that the ancient Greeks believed made up the body and determined illness?"},
+    {'question': "Who sent the Spanish Armada to England in 1588?"},
+    {'question': "Which English king built castles in the 13th century to help conquer Wales?"},
+    {'question': "The Chinese Exclusion Act was signed into law by which US president in 1882?"},
+    {'question': "Which 19th-century Englishwoman became the first qualified medical doctor?"},
+    {'question': "Which part of Berlin was enclosed by the wall?"},
+
+]
+
+answers = [
+
+    {'answer': "Anne of Cleves"},
+    {'answer': "Yasuke is known as the first foreign-born samurai in 16th-century Japan"},
+    {'answer': "Geoffrey of Monmouth"},
+    {'answer': "Harriet Tubman served in the America Civil War"},
+    {'answer': "It was the site of a failed attempt by a group of Cuban émigrés, with the backing of the US "
+               "government, to invade the island in 1961."},
+    {'answer': "Eleanor of Aquitaine"},
+    {'answer': "Soviet cosmonaut Yuri Gagarin, in April 1961"},
+    {'answer': "The body of Oliver Cromwell was exhumed in 1661."},
+    {'answer': "Edward the Elder, son of Alfred and Ealhswith of Mercia"},
+    {'answer': "Edward Teach is better known to history as the notorious 17th-century pirate ‘Blackbeard’"},
+    {'answer': "The Ides of March"},
+    {'answer': "In Thomas Farriner’s bakery on Pudding Lane (though technically the bakehouse was not located on "
+               "Pudding Lane proper, but on Fish Yard, a small enclave off Pudding Lane) | Learn more with this guide "
+               "to the Great Fire of London"},
+    {'answer': "The Waltz"},
+    {'answer': "King William IV (who was Victoria’s uncle)"},
+    {'answer': "Bristol | Read more about the Bristol bus boycott"},
+    {'answer': "Aaron Burr, the sitting vice president of the USA | Read more about the Hamilton-Burr duel"},
+    {'answer': "The drunkard’s cloak was a form of humiliating punishment used in the past for people who were "
+               "perceived to have abused alcohol"},
+    {'answer': "Cuneiform, an ancient writing system that was first used in around 3400 BC"},
+    {'answer': "Agrippina the Younger"},
+    {'answer': "Sarah Breedlove – who later became known as Madam CJ Walker"},
+    {'answer': "Collecting fossils, she was a palaeontologist"},
+    {'answer': "Edmund Spenser, in his epic poem ‘The Faerie Queene’"},
+    {'answer': "Countess Markievicz"},
+    {'answer': "Corsica"},
+    {'answer': "Utah; Omaha; Gold; Juno and Sword"},
+    {'answer': "Roanoke"},
+    {'answer': "The Peterloo Massacre"},
+    {'answer': "The Foo Fighters"},
+    {'answer': "1913"},
+    {'answer': "A battle formation that consisted of soldiers with long spears placed into circular, tightly packed "
+               "formations"},
+    {'answer': "Edward the Confessor"},
+    {'answer': "Michael Collins"},
+    {'answer': "He was a witch-finder"},
+    {'answer': "The Peasants' Revolt took place in 1381, in the 14th century"},
+    {'answer': "Malcolm X"},
+    {'answer': "Catherine of Aragon"},
+    {'answer': "The drilling of holes in the head and scraping or cutting of the skull"},
+    {'answer': "1840s"},
+    {'answer': "Robert the Bruce"},
+    {'answer': "Blood, phlegm, black bile, yellow bile"},
+    {'answer': "Philip II of Spain"},
+    {'answer': "Edward I"},
+    {'answer': "Chester A Arthur"},
+    {'answer': "Elizabeth Garrett Anderson"},
+    {'answer': "The west"}
+
+]
