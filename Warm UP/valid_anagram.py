fa4cd3b702a3dbccb8d8393fac695179292c027b
EnesBrt <enes.barut@gmx.com>
2023-07-30 20:44:39 +0200

    add valid_anagram.py

--- Warm UP/valid_anagram.py
+++ Warm UP/valid_anagram.py
@@ -1,10 +1,26 @@
 def find_anagram(s, t):
-    for x in s:
-        for y in t:
-            if y in s:
-                return True
-            else:
-                return False
+    char_count_s = {}
+    char_count_t = {}
 
+    for x in range(len(s)):
 
-print(find_anagram("car","rat"))
+        if s[x] in char_count_s:
+            char_count_s[s[x]] += 1
+        else:
+            char_count_s[s[x]] = 1
+    
+
+    for y in range(len(t)):
+
+        if t[y] in char_count_t:
+            char_count_t[t[y]] += 1
+        else:
+            char_count_t[t[y]] = 1
+    
+
+    if char_count_s == char_count_t:
+        return True
+    else:
+        return False
+
+print(find_anagram("car","rac"))
