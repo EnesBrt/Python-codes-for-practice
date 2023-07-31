405beb2ba90c6beedb6b9e3c4d4b93397bd7cfdb
EnesBrt <enes.barut@gmx.com>
2023-07-31 16:59:53 +0200

    update FirstUniqueCharacter.py

--- Warm UP/FirstUniqueCharacter.py
+++ Warm UP/FirstUniqueCharacter.py
@@ -1,24 +1,37 @@
+# Fonction pour trouver le premier caractère unique dans une chaîne
+
 
 def first_unique_charatcter(string):
 
-    indices_non_unique_char = {}
-    indices_unique_chars = {}
+    # Initialisation de deux dictionnaires pour stocker les indices des caractères
+    indices_non_unique_char = {}  # Pour les caractères qui apparaissent plus d'une fois
+    indices_unique_chars = {}  # Pour les caractères qui apparaissent une seule fois
 
+    # Parcourir chaque caractère dans la chaîne
     for x in range(len(string)):
+        # Si le caractère est déjà dans indices_non_unique_char, ajouter l'indice à sa liste
         if string[x] in indices_non_unique_char:
             indices_non_unique_char[string[x]].append(x)
         else:
-            # Check if the character is already in indices_unique_chars
+            # Si le caractère est déjà dans indices_unique_chars
             if string[x] in indices_unique_chars:
-                # If it is, move it to indices_non_unique_char
+                # Le déplacer à indices_non_unique_char et ajouter l'indice à sa liste
                 indices_non_unique_char[string[x]] = indices_unique_chars[string[x]]
                 indices_non_unique_char[string[x]].append(x)
-                # And remove it from indices_unique_chars
+                # Et le supprimer de indices_unique_chars
                 del indices_unique_chars[string[x]]
             else:
-                # If it's not in indices_unique_chars, create a new list for it
+                # Si le caractère n'est pas encore dans indices_unique_chars, créer une nouvelle liste pour lui
                 indices_unique_chars[string[x]] = [x]
 
-    return indices_unique_chars
+    # Obtenir le premier caractère unique (la clé du premier élément dans indices_unique_chars)
+    first_key = next(iter(indices_unique_chars))
+    # Obtenir l'indice du premier caractère unique
+    first_value = indices_unique_chars[first_key]
 
-print(first_unique_charatcter("loveleetcode"))
+    # Retourner l'indice du premier caractère unique
+    return first_value
+
+
+# Tester la fonction avec la chaîne "leetcode", le premier caractère unique est 'l' à l'indice 0
+print(first_unique_charatcter("leetcode"))
