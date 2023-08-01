0f8df7e899f8ea21881579406c732e9f5c43ec80
EnesBrt <enes.barut@gmx.com>
2023-08-01 10:02:46 +0200

    add traiing.py

---
+++ Warm UP/training.py
@@ -0,0 +1,16 @@
+def three_numbers():
+    
+    numbers = []
+
+    for x in range(10):
+        for y in range(x, 10):
+            for z in range(y , 10):
+                numbers.append(f"{x}{y}{z}")
+
+    result = ', '.join(numbers)
+
+    print(result)
+
+
+
+three_numbers()
